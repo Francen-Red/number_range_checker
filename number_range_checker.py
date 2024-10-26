60aec0d966b474471c1b30b53dd0c169addcd24f
Francen-Red <francenvenissered@gmail.com>
2024-10-27 01:38:25 +0800

    Pseudocode for the program

---
+++ number_range_checker.py
@@ -0,0 +1,24 @@
+# Create an empty list to store inputted numbers
+
+# Create a list that only accepts numbers 1-50 and adds the current valid number to it
+
+# Initialize counters for each range
+
+# Create a while loop for the inputted number to check its validity
+        # Ask user to input a number and convert it to integer
+
+        # Check the validity of the number
+        # If the inputted number is within the range of acceptable_numbers, append the number to the empty list and continue
+        # If not, print an error message and break the loop
+
+# Create a for loop to iterate over each number in the list
+    # Use if-statements to check which range the current number falls into
+    # Increment to corresponding range counter
+
+# Display the number of input for each range
+
+       
+
+
+
+       
