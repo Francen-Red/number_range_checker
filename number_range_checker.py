f855f425071356cc135381b9a6b16e6c04c45473
Francen-Red <francenvenissered@gmail.com>
2024-10-27 02:10:46 +0800

    Created a while loop for the inputted number that checks its validity and breaks the loop if input is invalid

--- number_range_checker.py
+++ number_range_checker.py
@@ -12,11 +12,18 @@ fourth_range = 0
 fifth_range = 0
 
 # Create a while loop for the inputted number to check its validity
+while True:
         # Ask user to input a number and convert it to integer
-
+        input_number = int(input("Please input a number between 1 and 50: "))
         # Check the validity of the number
         # If the inputted number is within the range of acceptable_numbers, append the number to the empty list and continue
-        # If not, print an error message and break the loop
+        if input_number in acceptable_numbers:
+            inputted_numbers.append(input_number)
+            continue
+        # Else, print an error message and break the loop
+        else:
+            print("Invalid input! Please enter a number ranging from 1-50")
+        break
 
 # Create a for loop to iterate over each number in the list
     # Use if-statements to check which range the current number falls into
